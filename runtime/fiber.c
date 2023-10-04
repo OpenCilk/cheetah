1ad66f5db55d02f729fd8be54411ead691def6f7
John F. Carr <jfc@mit.edu>
2023-10-04 15:54:02 -0400

    Use the stack guard page provided by the kernel

--- runtime/fiber.c
+++ runtime/fiber.c
@@ -1,4 +1,4 @@
-#ifndef _GNU_SOURCE
+#if defined __linux__ && !defined _GNU_SOURCE
 #define _GNU_SOURCE // For RTLD_DEFAULT from dlfcn.h
 #endif
 
@@ -17,19 +17,38 @@
 #include "fiber-header.h"
 #include "init.h"
 
-#include <string.h> /* DEBUG */
+#include <string.h> /* memset() */
+
+/* Set up flags for mmap to allocate a stack region.
+
+   Darwin does not have any special support for mapping stacks and
+   defines neither MAP_STACK nor MAP_GROWSDOWN.
+
+   On FreeBSD MAP_STACK requests a region that grows on demand and has
+   a guard page at the low end.  MAP_GROWSDOWN is not defined.
+   Strictly speaking, there are security.bsd.stack_guard_page guard
+   pages.  The default value of this tunable is 1.
+
+   On OpenBSD MAP_STACK designates the region as a legal stack location.
+   With some kernel configurations a program is not allowed to run on a
+   stack that has not been allocated with this flag.   MAP_GROWSDOWN is
+   not defined.
+
+   On Linux MAP_STACK does nothing and is provided for BSD compatibility.
+   MAP_GROWSDOWN has the same meaning as FreeBSD's MAP_STACK.  There is
+   a single guard page at the bottom of the region, and stack_guard_gap
+   unmapped pages between any two stacks. */
 
 #ifndef MAP_GROWSDOWN
-/* MAP_GROWSDOWN is implied on BSD */
 #define MAP_GROWSDOWN 0
 #endif
-#ifndef MAP_STACK
-/* MAP_STACK is not available on Darwin */
-#define MAP_STACK 0
+#ifdef MAP_STACK
+#define MAP_STACK_FLAGS \
+  (MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK | MAP_GROWSDOWN)
+#else
+#define MAP_STACK_FLAGS \
+  (MAP_PRIVATE | MAP_ANONYMOUS)
 #endif
-
-#define LOW_GUARD_PAGES 1
-#define HIGH_GUARD_PAGES 1
 
 //===============================================================
 // This file maintains fiber-related function that requires
@@ -263,11 +282,7 @@ static void make_stack(struct cilk_fiber *f, size_t stack_size) {
     const size_t page_size = 1U << page_shift;
 
     size_t stack_pages = (stack_size + page_size - 1) >> cheetah_page_shift;
-    // Overallocate pages so we can get a stack aligned to stack_size.
-    stack_pages += LOW_GUARD_PAGES + HIGH_GUARD_PAGES;
 
-    /* Stacks must be at least MIN_NUM_PAGES_PER_STACK pages,
-       a count which includes two guard pages. */
     if (stack_pages < MIN_NUM_PAGES_PER_STACK) {
         stack_pages = MIN_NUM_PAGES_PER_STACK;
     } else if (stack_pages > MAX_NUM_PAGES_PER_STACK) {
@@ -275,7 +290,7 @@ static void make_stack(struct cilk_fiber *f, size_t stack_size) {
     }
     char *alloc_low = (char *)mmap(
         0, stack_pages * page_size, PROT_READ | PROT_WRITE,
-        MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK | MAP_GROWSDOWN, -1, 0);
+        MAP_STACK_FLAGS, -1, 0);
     if (MAP_FAILED == alloc_low) {
         cilkrts_bug(NULL, "Cilk: stack mmap failed");
         /* Currently unreached.  TODO: Investigate more graceful
@@ -283,34 +298,29 @@ static void make_stack(struct cilk_fiber *f, size_t stack_size) {
         f->alloc_low = NULL;
         f->stack_low = NULL;
         f->stack_high = NULL;
-        f->alloc_high = NULL;
         return;
     }
-    char *alloc_high = alloc_low + stack_pages * page_size;
-    // Round down to the previous stack size.
-    char *stack_high = alloc_high - page_size;
+    char *stack_high = alloc_low + stack_pages * page_size;
     char *stack_low = alloc_low + page_size;
-    // mprotect guard pages.
-    mprotect(alloc_low, page_size, PROT_NONE);
-    mprotect(stack_high, page_size, PROT_NONE);
+#ifndef MAP_STACK
+    (void)mprotect(alloc_low, page_size, PROT_NONE);
+#endif
     f->alloc_low = alloc_low;
     f->stack_low = stack_low;
     f->stack_high = stack_high;
-    f->alloc_high = alloc_high;
     if (DEBUG_ENABLED(MEMORY_SLOW))
-        memset(stack_low, 0x11, stack_size);
+        memset(stack_low, 0x11, stack_high - stack_low);
 }
 
 static void free_stack(struct cilk_fiber *f) {
     if (f->alloc_low) {
         if (DEBUG_ENABLED(MEMORY_SLOW))
             memset(f->stack_low, 0xbb, f->stack_high - f->stack_low);
-        if (munmap(f->alloc_low, f->alloc_high - f->alloc_low) < 0)
+        if (munmap(f->alloc_low, f->stack_high - f->alloc_low) < 0)
             cilkrts_bug(NULL, "Cilk: stack munmap failed");
         f->alloc_low = NULL;
         f->stack_low = NULL;
         f->stack_high = NULL;
-        f->alloc_high = NULL;
     }
 }
 
@@ -318,7 +328,6 @@ static void fiber_init(struct cilk_fiber *fiber) {
     fiber->alloc_low = NULL;
     fiber->stack_low = NULL;
     fiber->stack_high = NULL;
-    fiber->alloc_high = NULL;
 }
 
 
