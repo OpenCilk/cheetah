6b1e2e3abf4d0a48a679515142705cc42cb72ac9
John F. Carr <jfc@mit.edu>
2025-06-04 05:25:51 -0400

    simplify

--- runtime/cilk2c.h
+++ runtime/cilk2c.h
@@ -9,8 +9,8 @@
 
 #include "rts-config.h"
 
-struct __cilkrts_stack_frame;
-struct __cilkrts_worker;
+typedef struct __cilkrts_stack_frame __cilkrts_stack_frame;
+typedef struct __cilkrts_worker __cilkrts_worker;
 
 // Returns 1 if the current exection is running on Cilk workers, 0 otherwise.
 CHEETAH_API int __cilkrts_running_on_workers(void);
@@ -21,31 +21,28 @@ CHEETAH_API int __cilkrts_running_on_workers(void);
 
 // Check if the runtime is storing an exception we need to handle later, and
 // raises that exception if so.
-CHEETAH_API void __cilkrts_check_exception_raise(struct __cilkrts_stack_frame *sf);
+CHEETAH_API void __cilkrts_check_exception_raise(__cilkrts_stack_frame *sf);
 
 // Check if the runtime is storing an exception we need to handle later, and
 // resumes unwinding with that exception if so.
-CHEETAH_API void __cilkrts_check_exception_resume(struct __cilkrts_stack_frame *sf);
+CHEETAH_API void __cilkrts_check_exception_resume(__cilkrts_stack_frame *sf);
 
 // Performs runtime operations to handle a cilk_sync.
 __attribute__((noreturn, nothrow))
-CHEETAH_API void __cilkrts_sync(struct __cilkrts_stack_frame *sf);
+CHEETAH_API void __cilkrts_sync(__cilkrts_stack_frame *sf);
 
 // Inserted on return from a spawning function that is not itself a spawn
 // helper.  Performs Cilk's return protocol for such functions.
-CHEETAH_API void __cilkrts_leave_frame(struct __cilkrts_stack_frame *sf);
+CHEETAH_API void __cilkrts_leave_frame(__cilkrts_stack_frame *sf);
 
 // Called from __cilkrts_enter_landingpad to optionally fix the current stack
 // pointer and cleanup a fiber that was previously saved for exception handling.
-CHEETAH_API
-void __cilkrts_cleanup_fiber(struct __cilkrts_stack_frame *, int32_t sel);
+CHEETAH_API void __cilkrts_cleanup_fiber(__cilkrts_stack_frame *, int32_t sel);
 
 // Not marked as CHEETAH_API as it may be deprecated soon
 unsigned __cilkrts_get_nworkers(void);
 
-CHEETAH_API
-void __cilkrts_set_return(struct __cilkrts_worker *const ws);
-CHEETAH_API
-void __cilkrts_exception_handler(struct __cilkrts_worker *w, char *exn);
+CHEETAH_API void __cilkrts_set_return(__cilkrts_worker *const ws);
+CHEETAH_API void __cilkrts_exception_handler(__cilkrts_worker *w, char *exn);
 
 #endif
