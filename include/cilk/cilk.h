1c2bd0d244b24a2af77ffecc3be0bc46274cb9f8
John F. Carr <jfc@mit.edu>
2026-01-23 09:53:05 -0700

    Keyword renaming is not needed if __cilk >= 301

--- include/cilk/cilk.h
+++ include/cilk/cilk.h
@@ -1,11 +1,15 @@
 #ifndef _CILK_H
 #define  _CILK_H
 
+#if __cilk < 301
+
 #define cilk_spawn _Cilk_spawn
 #define cilk_sync  _Cilk_sync
 #define cilk_for   _Cilk_for
 #define cilk_scope   _Cilk_scope
 
 #define cilk_reducer _Hyperobject
+
+#endif
 
 #endif /* _CILK_H */
