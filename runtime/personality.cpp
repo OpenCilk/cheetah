52d390d00300af655740194fdcb3200009db93df
John F. Carr <jfc@mit.edu>
2026-04-13 17:11:33 -0400

    Put both exception personalities into the main runtime library

--- runtime/personality.cpp
+++ runtime/personality.cpp
@@ -22,6 +22,18 @@ typedef _Unwind_Reason_Code (*__personality_routine)(
     int version, _Unwind_Action actions, uint64_t exception_class,
     _Unwind_Exception *exception_object, _Unwind_Context *context);
 
+extern "C"
+_Unwind_Reason_Code __gcc_personality_v0(int version, _Unwind_Action actions,
+                                         uint64_t exception_class,
+                                         struct _Unwind_Exception *ue_header,
+                                         struct _Unwind_Context *context);
+extern "C"
+_Unwind_Reason_Code __gxx_personality_v0(int version, _Unwind_Action actions,
+                                         uint64_t exception_class,
+                                         struct _Unwind_Exception *ue_header,
+                                         struct _Unwind_Context *context);
+
+
 static char *get_cfa(_Unwind_Context *context) {
     /* _Unwind_GetCFA is originally a gcc extension.  FreeBSD has its
        own library without that extension. */
@@ -313,4 +325,31 @@ extern "C" _Unwind_Reason_Code __cilk_personality_internal(
     } else {
         return _URC_FATAL_PHASE1_ERROR;
     }
+}
+
+extern "C" {
+_Unwind_Reason_Code __cilk_personality_c_v0(int version, _Unwind_Action actions,
+                                            uint64_t exception_class,
+                                            struct _Unwind_Exception *ue_header,
+                                            struct _Unwind_Context *context) {
+    return __cilk_personality_internal(__gcc_personality_v0, version, actions,
+                                       exception_class, ue_header, context);
+}
+
+// Legacy name
+_Unwind_Reason_Code __cilk_personality_v0(int version, _Unwind_Action actions,
+                                          uint64_t exception_class,
+                                          struct _Unwind_Exception *ue_header,
+                                          struct _Unwind_Context *context) {
+    return __cilk_personality_c_v0(version, actions, exception_class, ue_header,
+                                   context);
+}
+
+_Unwind_Reason_Code __cilk_personality_cpp_v0(
+    int version, _Unwind_Action actions, uint64_t exception_class,
+    struct _Unwind_Exception *ue_header, struct _Unwind_Context *context) {
+    return __cilk_personality_internal(__gxx_personality_v0, version, actions,
+                                       exception_class, ue_header, context);
+}
+
 }
